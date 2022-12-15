02ad82742e06ec013862d0aac0ca26d8607be228
unknown <talakhatib4@gmail.com>
2022-12-15 15:24:18 +0200

    change weather api

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,7 +4,7 @@ class WeatherController < ApplicationController
     def search 
         ip = remote_ip()
         city = Geocoder.search(ip).first.city
-        uri = URI('http://api.weatherstack.com/current?access_key=be23d19041bdfe894b1589ea3b4736c4&query='+city)
+        uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+city)
         res = Net::HTTP.get_response(uri) 
         @data = JSON.parse(res.body)
         @searches = CityWeather.arrange_desc.first(10)
@@ -13,15 +13,15 @@ class WeatherController < ApplicationController
     def api_data 
         city_name = params[:city_name]
         if !city_name.blank?
-        uri = URI('http://api.weatherstack.com/current?access_key=be23d19041bdfe894b1589ea3b4736c4&query='+params[:city_name])
+        uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+params[:city_name])
         res = Net::HTTP.get_response(uri) 
         @data = JSON.parse(res.body)
-        country = @data["location"]["country"]
-        name = @data["location"]["name"]
-        temperature = @data["current"]["temperature"]
-        weather_description = @data["current"]["weather_descriptions"].first
-        wind_speed = @data["current"]["wind_speed"]
-        wind_degree = @data["current"]["wind_degree"]
+        country = @data["data"]["country_code"]
+        name = @data["data"]["city_name"]
+        temperature = @data["data"]["temp"]
+        weather_description = @data["data"]["weather"]["description"]
+        wind_speed = @data["data"]["wind_spd"]
+        wind_degree = @data["data"]["wind_dir"]
         city = CityWeather.new(country: country,name: name ,temperature: temperature,weather_description: weather_description,wind_speed: wind_speed,wind_degree: wind_degree)
         city.save
         searches = CityWeather.arrange_desc.first(11)
--- app/mailers/weather_report_mailer.rb
+++ app/mailers/weather_report_mailer.rb
@@ -4,15 +4,15 @@ class WeatherReportMailer < ApplicationMailer
         @user = user
         city_user = CityUser.find_by(user: user)
         city_name = city_user.his_city
-        uri = URI('http://api.weatherstack.com/current?access_key=be23d19041bdfe894b1589ea3b4736c4&query='+city_name)
+        uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+city_name)
         res = Net::HTTP.get_response(uri) 
         @data = JSON.parse(res.body)
-        @country = @data["location"]["country"]
-        @name = @data["location"]["name"]
-        @temperature = @data["current"]["temperature"]
-        @weather_description = @data["current"]["weather_descriptions"].first
-        @wind_speed = @data["current"]["wind_speed"]
-        @wind_degree = @data["current"]["wind_degree"]
+        @country = @data["data"]["country_code"]
+        @name = @data["data"]["city_name"]
+        @temperature = @data["data"]["temp"]
+        @weather_description = @data["data"]["weather"]["description"]
+        @wind_speed = @data["data"]["wind_spd"]
+        @wind_degree = @data["data"]["wind_dir"]
 
         mail to: @user.email , subject: "Daily Weather Report"
     end
