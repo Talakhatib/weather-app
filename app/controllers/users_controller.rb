c758f2e9ed686ce3fd89784b942035fa99c5e575
unknown <talakhatib4@gmail.com>
2023-01-06 13:39:02 +0200

    adding api's

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,16 @@ class UsersController < ApplicationController
      def create
       ip = remote_ip()
       city = Geocoder.search(ip).first.city
-      @user = User.new(email: params[:email], city: city)
-      if @user.save
-        # WeatherReportMailer.daily_weather_report(@user).deliver_now
-        WeatherReportMailerJob.perform_now
+      uri = URI('http://host.docker.internal:3001/users')
+      res = Net::HTTP.post_form(uri,{"email"=>params[:email],"city"=>city})
+      result = JSON.parse(res.body)
+      if result["error"].present?
+         flash[:danger]= result["error"]
+         redirect_to root_path
+      else
         flash[:success] = "Your subscription is successfully done !! Check your email . "
         redirect_to root_path
-      else 
-         flash[:danger]="Invalid Email! Please try again."
-         redirect_to root_path
       end
-        
-     end
-
-     def unsubscribe
-      user = User.find(params[:user])
-      user.destroy 
-      flash[:success] = "You Unsubscribe !"
-      redirect_to root_path
      end
 
 end
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,63 +2,64 @@ require 'net/http'
 class WeatherController < ApplicationController
 
     def search 
-        @searches = CityWeather.arrange_desc.first(10)
+        uri = URI('http://host.docker.internal:3001/searches?number_of_searches=10')
+        res = Net::HTTP.get_response(uri)
+        @searches = JSON.parse(res.body)
         ip = remote_ip()
         result = Geocoder.search(ip)
-        #flash[:danger] = "safdsad" if !result.first.city.present?
-        if result.first.city.present?
-          city = result.first.city
-          uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+city)
-          res = Net::HTTP.get_response(uri)
-          if  res.class == Net::HTTPOK 
-             data = JSON.parse(res.body)
-             @country = data["data"].first["country_code"]
-             @localtime = data["data"].first["datetime"]
-             @name = data["data"].first["city_name"]
-             @temperature = data["data"].first["temp"]
-             @weather_description = data["data"].first["weather"]["description"]
-             @wind_speed = data["data"].first["wind_spd"]
-             @wind_degree = data["data"].first["wind_dir"]
-          else 
-            flash[:danger] = "No result is found for your location !!"
-          end
-        else 
-            flash[:danger] = "City is not found !"
-        end
-    end
-
-    def api_data 
-        city_name = params[:city_name]
-        # if city_name.blank? 
-        #   flash[:danger]="You need to enter a city name"
-        #   redirect_to root_path
-        # end
-        if !city_name.blank?
-           uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+params[:city_name])
-           res = Net::HTTP.get_response(uri) 
-          if  res.class == Net::HTTPOK
+        flash[:danger] = "City is not found !" if !result.first.city.present?
+        city = result.first.city
+        uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+city)
+        res = Net::HTTP.get_response(uri)
+        if  res.class == Net::HTTPOK 
             data = JSON.parse(res.body)
             @country = data["data"].first["country_code"]
+            @localtime = data["data"].first["datetime"]
             @name = data["data"].first["city_name"]
             @temperature = data["data"].first["temp"]
             @weather_description = data["data"].first["weather"]["description"]
             @wind_speed = data["data"].first["wind_spd"]
             @wind_degree = data["data"].first["wind_dir"]
-            CityWeather.create(country: @country,name: @name ,temperature: @temperature,weather_description: @weather_description,wind_speed: @wind_speed,wind_degree: @wind_degree)
-            searches = CityWeather.arrange_desc.first(11)
-            @searches = searches[1..10]# exclude the last element
-            respond_to do |format|
-             format.turbo_stream
-             format.html { redirect_to root_path }
-            end
-          else 
-            flash[:danger] = "You need to enter a uncorrect city name .Please try again !!"
+        else 
+            flash[:danger] = "No result is found for your location !!"
+        end
+    end
+
+    def api_data 
+        city_name = params[:city_name]
+        if city_name.blank? 
+          flash[:danger]="You need to enter a city name"
+          redirect_to root_path
+        end
+        uri = URI('https://api.weatherbit.io/v2.0/current?key=36aa05d409f143bebfd437e6ecc5cbc8&city='+params[:city_name])
+        res = Net::HTTP.get_response(uri) 
+        if  res.class == Net::HTTPOK
+          data = JSON.parse(res.body)
+          @country = data["data"].first["country_code"]
+          @name = data["data"].first["city_name"]
+          @temperature = data["data"].first["temp"]
+          @weather_description = data["data"].first["weather"]["description"]
+          @wind_speed = data["data"].first["wind_spd"]
+          @wind_degree = data["data"].first["wind_dir"]
+          uri = URI('http://host.docker.internal:3001/city_weathers')
+          res = Net::HTTP.post_form(uri,{'country'=>@country,'name'=>@name,'temperature'=>@temperature.to_s,'weather_description'=>@weather_description,'&wind_speed'=>@wind_speed.to_s,'wind_degree'=>@wind_degree.to_s})
+          result = JSON.parse(res.body)
+          if result["error"].present?
+            flash[:danger] = result["error"]
             redirect_to root_path
           end
-       else
-        flash[:danger]="You need to enter a city name"
-        redirect_to root_path
-       end
+          uri = URI('http://host.docker.internal:3001/searches?number_of_searches=11')
+          res = Net::HTTP.get_response(uri)
+          searches_data = JSON.parse(res.body)
+          @searches = searches_data[1..10]# exclude the last element
+          respond_to do |format|
+            format.turbo_stream
+            format.html { redirect_to root_path }
+          end
+        else 
+          flash[:danger] = "You need to enter a uncorrect city name .Please try again !!"
+          redirect_to root_path
+        end
      end
 
 end
